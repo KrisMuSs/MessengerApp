d65dd50118be33c9521274bcb42affd7d8ccabd0
KrisMuSs <artem.merzlikin.67@mail.ru>
2025-02-13 10:11:03 +0700

    Created the login interface

---
+++ Messenger/App/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Артем Мерзликин on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
