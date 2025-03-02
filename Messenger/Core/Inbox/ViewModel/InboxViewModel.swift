852df4677385c73c12656217117c0125e3212bc2
KrisMuSs <artem.merzlikin.67@mail.ru>
2025-03-02 10:16:45 +0700

    Bugfix with duplicate messages in the Inbox

--- Messenger/Core/Inbox/ViewModel/InboxViewModel.swift
+++ Messenger/Core/Inbox/ViewModel/InboxViewModel.swift
@@ -20,30 +20,45 @@ class InboxViewModel: ObservableObject{
     
     // Метод настраивает наблюдение за изменениями в userSession
     private func setupSubscribers() {
+       // Подписка позволяет обновлять информацию о текущем пользователе, когда она изменяется, и обновлять состояние currentUser
         UserService.shared.$currentUser.sink { [weak self] user in
             self?.currentUser = user
         }.store(in: &cancellables)
         
+        //Подписка позволяет отслеживать изменения в списке сообщений и автоматически обновлять состояние recentMessages, когда появляются новые сообщения или изменяются существующие
         service.$documentChanges.sink { [weak self] changes in
             self?.loadInditialessages(fromChanges: changes)
         }.store(in: &cancellables)
         
     }
-    
+    /// Загружает и обновляет список сообщений, добавив информацию о собеседнике для каждого сообщения
     private func loadInditialessages(fromChanges changes: [DocumentChange]) {
         // Вернет нам текст и время отправки сообщения, но не вернет данные пользователя
         var messages = changes.compactMap({ try? $0.document.data(as: Message.self) })
         
-        for i in 0 ..< messages.count{
-            let message = messages[i]
-            
-            // возвращаем пользователя
-            UserService.fetchUser(withUid: message.chatPartnerId) { user in
-                messages[i].user = user
-                // Добавляем его в список recentMessages. Теперь у нас есть текст, время отправки и пользователь
-                self.recentMessages.append(messages[i])
+        // Если сообщения есть, обрабатываем
+        if !messages.isEmpty {
+            for i in 0 ..< messages.count {
+                let message = messages[i]
+                
+                // Находим старое сообщение этого пользователя, если оно уже есть в списке
+                if let index = recentMessages.firstIndex(where: { $0.chatPartnerId == message.chatPartnerId }) {
+                    // Удаляем старое сообщение этого пользователя
+                    recentMessages.remove(at: index)
+                }
+                
+                // Возвращаем пользователя для текущего сообщения
+                UserService.fetchUser(withUid: message.chatPartnerId) { user in
+                    messages[i].user = user
+                    // Добавляем новое сообщение в список recentMessages
+                    self.recentMessages.append(messages[i])
+                    
+                    // Сортируем список по времени (от нового к старому)
+                    self.recentMessages.sort { $0.timestamp.compare($1.timestamp) == .orderedDescending }
+                }
             }
         }
     }
+
     
 }
